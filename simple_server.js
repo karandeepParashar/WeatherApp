7a33e107ab7f487996420e526cf06044261f705d
karandeep <karan5parashar5@gmail.com>
2022-04-04 01:21:37 -0400

    Modified css files and added switch case for routes

--- simple_server.js
+++ simple_server.js
@@ -1,37 +1,44 @@
 //importing the http module and setting up connection parameters
-// import http from "http";
-// import fs from "fs";
 const http = require("http");
 const fs = require("fs");
 const host = "localhost";
 const port = 3000;
 
-//router for the server
-const router = function (req, res) {
-  const path = req.url;
-  console.log(path);
-  if (path == "/my-weather-app/single-search" || path == "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    fs.createReadStream("templates/index.html").pipe(res);
-  } else if (path == "/static/style/single_city.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    fs.createReadStream("static/style/single_city.css").pipe(res);
-  } else if (path == "/static/scripts/single_city.js") {
-    res.writeHead(200, { "content-type": "text/js" });
-    fs.createReadStream("static/scripts/single_city.js").pipe(res);
-  } else if (path == "/my-weather-app/multiple-search") {
-    res.writeHead(200, { "content-type": "text/html" });
-    fs.createReadStream("templates/mutlipleCity.html").pipe(res);
-  } else if (path == "/static/style/multiple_cities.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    fs.createReadStream("static/style/multiple_cities.css").pipe(res);
-  } else if (path == "/static/scripts/multiple_cities.js") {
-    res.writeHead(200, { "content-type": "text/js" });
-    fs.createReadStream("static/scripts/multiple_cities.js").pipe(res);
+//routes for incoming requests
+const routes = function (req, res) {
+  switch (req.url) {
+    case "/my-weather-app/single-search":
+      res.writeHead(200, { "content-type": "text/html" });
+      fs.createReadStream("templates/index.html").pipe(res);
+      break;
+    case "/my-weather-app/multiple-search":
+      res.writeHead(200, { "content-type": "text/html" });
+      fs.createReadStream("templates/mutlipleCity.html").pipe(res);
+      break;
+    case "/static/style/single_city.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("static/style/single_city.css").pipe(res);
+      break;
+    case "/static/style/multiple_cities.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("static/style/multiple_cities.css").pipe(res);
+      break;
+    case "/static/scripts/single_city.js":
+      res.writeHead(200, { "content-type": "text/js" });
+      fs.createReadStream("static/scripts/single_city.js").pipe(res);
+      break;
+    case "/static/scripts/multiple_cities.js":
+      res.writeHead(200, { "content-type": "text/js" });
+      fs.createReadStream("static/scripts/multiple_cities.js").pipe(res);
+      break;
+    default:
+      res.writeHead(200, { "content-type": "text/html" });
+      fs.createReadStream("templates/index.html").pipe(res);
   }
 };
+
 //creating the server and start listening
-const server = http.createServer(router);
+const server = http.createServer(routes);
 server.listen(port, host, () => {
   console.log(
     `Server is running on http://${host}:${port}/my-weather-app/single-search`
